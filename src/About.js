79ec9f5b127b8518ab0e65250823e258439ad1df
Rebecca Hagerty <rebecca.hagerty12@gmail.com>
2021-09-14 14:06:11 -0400

    style updates

--- src/About.js
+++ src/About.js
@@ -27,6 +27,7 @@ const About = () => {
         <Row className="about-wrapper">
           <Col lg={5} md={12} sm={12}>
             <Fade bottom duration={1000} delay={600} distance="30px">
+
               <div className="about-wrapper__image">
                 <img alt="profile picture" id="profilePic" src={Img} />
               </div>
@@ -43,26 +44,30 @@ const About = () => {
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
                   Hi! I'm Becca -- your friendly neighborhood computer nerd and
-                  self proclaimed idea machine. My fascination with technology, computers, and coding
-                  began before I can remember. As a preteen, I dabbled with HTML
-                  styling for my social media pages (LiveJournal or Myspace
-                  anyone?) and was an avid video gamer. It didn't take long
-                  before I discovered the kismet connection between my technical
-                  brain and computer savvy and made the decision to pursue software engineering. 
+                  self proclaimed idea machine. My fascination with technology,
+                  computers, and coding began before I can remember. As a
+                  preteen, I dabbled with HTML styling for my social media pages
+                  (LiveJournal or Myspace anyone?) and was an avid video gamer.
+                  It didn't take long before I discovered the kismet connection
+                  between my technical brain and computer savvy and made the
+                  decision to pursue software engineering.
                 </p>
                 <p className="about-wrapper__info-text">
-                  Fast forward to today, I have designed and developed
-                  numerous apps and websites. I've focused my education and my work on responsive
-                  development and accessible design. I'm an avid researcher, which allows me to more deeply understand my audience (or the problem) and provide a unique solution.
-                  
+                  Fast forward to today, I have designed and developed numerous
+                  apps and websites. I've focused my education and my work on
+                  responsive development and accessible design. I'm an avid
+                  researcher, which allows me to more deeply understand my
+                  audience (or the problem) and provide a unique solution.
                 </p>
                 <p className="about-wrapper__info-text">
-                  I have a 1 year old cat named Moosh, am an avid traveler,
-                  and nature and animal enthusiast. I am also a very community oriented
-                  person and spend as much time as possible volunteering and
-                  mentoring in Detroit. While I am an advocate for many causes,
-                  my primary focuses are human rights, community aid, and
-                  environmentalism. I'm currently developing a template for animal rescue shelters to use on their websites for embedded affiliate links.
+                  I have a 1 year old cat named Moosh, am an avid traveler, and
+                  nature and animal enthusiast. I am also a very community
+                  oriented person and spend as much time as possible
+                  volunteering and mentoring in Detroit. While I am an advocate
+                  for many causes, my primary focuses are human rights,
+                  community aid, and environmentalism. I'm currently developing
+                  a template for animal rescue shelters to use on their websites
+                  for embedded affiliate links.
                 </p>
               </div>
             </Fade>
