fba64a8d34f7361baee569cca953d60977909a1a
Rebecca Hagerty <rebecca.hagerty12@gmail.com>
2021-09-23 09:14:18 -0400

    updates

--- src/About.js
+++ src/About.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import Fade from "react-reveal/Fade";
 import { Container, Row, Col } from "react-bootstrap";
 import Img from "./images/me.jpg";
+import moosh from "./images/moosh1.jpg";
+import moosh2 from "./images/moosh2.jpg";
+import moosh3 from "./images/moosh3.jpg";
 
 const About = () => {
   const [isDesktop, setIsDesktop] = useState(false);
@@ -31,6 +34,11 @@ const About = () => {
               <div className="about-wrapper__image">
                 <img alt="profile picture" id="profilePic" src={Img} />
               </div>
+              <Row>
+                <Col lg={4} md={4} sm={4}><img alt="picture of cat" src={moosh} className="moosh"/></Col>
+                <Col lg={4} md={4} sm={4}><img alt="picture of cat" src={moosh2} className="moosh"/></Col>
+                <Col lg={4} md={4} sm={4}><img alt="picture of cat" src={moosh3} className="moosh"/></Col>
+              </Row>
             </Fade>
           </Col>
           <Col lg={7} md={12} sm={12}>
@@ -55,7 +63,7 @@ const About = () => {
                   audience (and the problem) and provide a unique solution.
                 </p>
                 <p className="about-wrapper__info-text">
-                Outside of computers, I have a 1-year-old cat named Moosh and am an avid traveler and nature enthusiast. I am a very community-oriented person and like to utilize my skillset to support local organizations. I volunteer and mentor young women in STEM on a regular basis. While I am an advocate
+                Outside of programming, I have a 1-year-old cat named Moosh (who also took a natural liking to computers) and am an avid traveler and nature enthusiast. I am a very community-oriented person and like to utilize my skillset to support local organizations. I volunteer and mentor young women in STEM on a regular basis. While I am an advocate
                   for many causes, my primary focuses are human rights,
                   community aid, and environmentalism. I'm currently developing a WordPress template for animal rescue shelters to use on their websites
                   for embedded affiliate links (for additional passive fundraising). I'm looking to further develop my skillset at a company that shares my values for making a positive impact.
